fb3b11a2b023c1c11fecdfec8031d9f563c180d8
marlisapee <mpee@tripadvisor.com>
2024-04-04 17:26:56 -0400

    added multiple rounds

--- src/game.js
+++ src/game.js
@@ -5,6 +5,9 @@ const player1Wins = 'Player 1 Wins!'
 const player2Wins = 'Player 2 Wins!'
 const tie = `It's a tie!`
 
+let p1Score = 0;
+let p2Score = 0;
+
 const randomChoice = () => {
     const gameMap = {
         0: 'rock',
@@ -15,26 +18,41 @@ const randomChoice = () => {
     return gameMap[choice];
 }   
 
-const displayMessage = (message) => console.log(message);
-    
 const winner = (player1, player2) => {
-    if(player1 === player2) return displayMessage(tie);
-    if(player1 === 'rock' && player2 === 'paper') return displayMessage(player2Wins)
-    if(player1 === 'rock' && player2 === 'scissors') return displayMessage(player1Wins)
-    if(player1 === 'paper' && player2 === 'rock') return displayMessage(player1Wins)
-    if(player1 === 'paper' && player2 === 'scissors') return displayMessage(player2Wins)
-    if(player1 === 'scissors' && player2 === 'rock') return displayMessage(player2Wins)
-    if(player1 === 'scissors' && player2 === 'paper') return displayMessage(player1Wins)
+    if(player1 === player2) return tie;
+    if(player1 === 'rock' && player2 === 'paper') return player2Wins
+    if(player1 === 'rock' && player2 === 'scissors') return player1Wins
+    if(player1 === 'paper' && player2 === 'rock') return player1Wins
+    if(player1 === 'paper' && player2 === 'scissors') return player2Wins
+    if(player1 === 'scissors' && player2 === 'rock') return player2Wins
+    if(player1 === 'scissors' && player2 === 'paper') return player1Wins
 }
 
-const startGame = () => {
+const startRound = () => {
     const player1 = randomChoice()
     const player2 = randomChoice()
 
     console.log('Player 1: ', player1);
     console.log('Player 2: ', player2);
-    
-    winner(player1, player2)
+
+    let result = winner(player1, player2)
+    console.log(result);
+    score(result)
+
+    console.log('p1 score: ', p1Score);
+    console.log('p2 score: ', p2Score);
 }
 
-startGame()
+const score = (result) => {
+    if(result === tie) return;
+    (result === player1Wins) ? p1Score++ : p2Score++
+}
+
+const startGame = (rounds) => {
+    for(let i = 1; i <= rounds; i++){
+        console.log(`Round ${i}`);
+        startRound()
+    }
+}
+
+startGame(10)
